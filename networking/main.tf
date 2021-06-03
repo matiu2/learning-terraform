d0c4c67b41007db7cd3b86f8a56e6b1744d461b1
Matthew Sherborne <msherborne@gmail.com>
2021-06-03 11:43:20 +1000

    Improvements to networking

--- networking/main.tf
+++ networking/main.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+// RDS (private) subnets
 resource "aws_route_table_association" "public-subnet-rta" {
   count          = var.public_subnet_count
   subnet_id      = aws_subnet.public_subnet[count.index].id
@@ -48,12 +49,23 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_db_subnet_group" "matiu-rds-subnets" {
+  count      = var.db_subnet_group ? 1 : 0
+  name       = "matiu-rds-subnet-group"
+  subnet_ids = aws_subnet.private_subnet.*.id
+  tags = {
+    Name = "matiu-rds-subnet-group"
+  }
+}
+
 resource "aws_internet_gateway" "matiu-igw" {
   vpc_id = aws_vpc.matiu_vpc.id
   tags = {
     Name = "matiu-igw"
   }
 }
+
+// Routing
 
 resource "aws_route_table" "matiu-public-rt" {
   vpc_id = aws_vpc.matiu_vpc.id
@@ -71,21 +83,10 @@ resource "aws_default_route_table" "matiu-default-rt" {
   tags                   = { Name = "matiu-default-rt" }
 }
 
+// Instances Security group ////
 resource "aws_security_group" "instance-sg" {
   name        = "instance-sg"
   description = "Security group for ec2 instances"
-  vpc_id      = aws_vpc.matiu_vpc.id
-}
-
-resource "aws_security_group" "lb-sg" {
-  name        = "lb-sg"
-  description = "Security group for the load balancer"
-  vpc_id      = aws_vpc.matiu_vpc.id
-}
-
-resource "aws_security_group" "rds-sg" {
-  name        = "rds-sg"
-  description = "Security group for the database"
   vpc_id      = aws_vpc.matiu_vpc.id
 }
 
@@ -98,6 +99,35 @@ resource "aws_security_group_rule" "instance-ssh" {
   cidr_blocks       = [var.ssh_access_cidr]
 }
 
+resource "aws_security_group_rule" "instance-kubectl" {
+  security_group_id = aws_security_group.instance-sg.id
+  type              = "ingress"
+  from_port         = 6443
+  to_port           = 6443
+  protocol          = "tcp"
+  cidr_blocks       = [var.ssh_access_cidr]
+}
+
+resource "aws_security_group_rule" "instance-http-out" {
+  // This is required at the start to install kubectl (when running the provisioners)
+  security_group_id = aws_security_group.instance-sg.id
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "instance-https-out" {
+  // This is required at the start to install kubectl (when running the provisioners)
+  security_group_id = aws_security_group.instance-sg.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "instance-loadbalancer" {
   // Allow the LB to talk to the instances on whatever port they're serving on
   security_group_id        = aws_security_group.instance-sg.id
@@ -108,8 +138,16 @@ resource "aws_security_group_rule" "instance-loadbalancer" {
   source_security_group_id = aws_security_group.lb-sg.id
 }
 
+// Load balancer security group /////
+
+resource "aws_security_group" "lb-sg" {
+  name        = "lb-sg"
+  description = "Security group for the load balancer"
+  vpc_id      = aws_vpc.matiu_vpc.id
+}
+
 resource "aws_security_group_rule" "lb-incoming" {
-  // Allow people to hit the load balancer on whatever port it's serving on
+  // Allow the world to hit the load balancer on whatever port it's serving on
   security_group_id = aws_security_group.lb-sg.id
   type              = "ingress"
   from_port         = var.public_lb_port
@@ -118,12 +156,29 @@ resource "aws_security_group_rule" "lb-incoming" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "lb-out" {
+  // Allow the load balancer to reach out to the instances
+  security_group_id        = aws_security_group.lb-sg.id
+  type                     = "egress"
+  from_port                = var.instance_to_lb_port
+  to_port                  = var.instance_to_lb_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.instance-sg.id
+}
 
-resource "aws_db_subnet_group" "matiu-rds-subnets" {
-  count      = var.db_subnet_group ? 1 : 0
-  name       = "matiu-rds-subnet-group"
-  subnet_ids = aws_subnet.private_subnet.*.id
-  tags = {
-    Name = "matiu-rds-subnet-group"
-  }
+/// RDS Security group ///
+
+resource "aws_security_group" "rds-sg" {
+  name        = "rds-sg"
+  description = "Security group for the database"
+  vpc_id      = aws_vpc.matiu_vpc.id
+}
+
+resource "aws_security_group_rule" "instances-to-rds" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.rds-sg.id
+  source_security_group_id = aws_security_group.instance-sg.id
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
 }
