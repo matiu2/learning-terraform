f7148c5a62c722024069bec37180cc9a28ffc03b
Matthew Sherborne <msherborne@gmail.com>
2021-06-03 11:43:47 +1000

    Comment out the default tags due to AWS provider bug

--- providers.tf
+++ providers.tf
@@ -9,9 +9,9 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  default_tags {
-    tags = {
-      Terraform = "Matthew_Learning"
-    }
-  }
+  # default_tags {
+  #   tags = {
+  #     Terraform = "Matthew_Learning"
+  #   }
+  # }
 }
